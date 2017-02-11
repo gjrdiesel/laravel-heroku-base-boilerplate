574a225fd7f699fc39168002caab24fa9eb1f7c3
Justin <knoxxbox@gmail.com>
2017-02-11 15:22:26 -0500

    added db parser

--- src/PaasServiceProvider.php
+++ src/PaasServiceProvider.php
@@ -14,6 +14,8 @@ class PaasServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->setupFromDatabaseUrl();
+
         $this->publishes([
             __DIR__.'/../stubs' => base_path(),
         ],'paas');
@@ -33,5 +35,38 @@ class PaasServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    private function setupFromDatabaseUrl()
+    {
+        $db_url = env('DATABASE_URL');
+
+        if(!$db_url){
+            return;
+        }
+
+        $db = parse_url($db_url);
+
+        $rewrite_schemes = [
+            'postgres' => 'pgsql'
+        ];
+
+        if(isset($rewrite_schemes[$db['scheme']])){
+            $db['scheme'] = $rewrite_schemes[$db['scheme']];
+        }
+
+        $db_config = [
+            'DB_CONNECTION' => $db['scheme'],
+            'DB_HOST' => $db['host'],
+            'DB_PORT' => $db['port'],
+            'DB_USERNAME' => $db['user'],
+            'DB_PASSWORD' => $db['pass'],
+            'DB_DATABASE' => str_replace('/','',$db['path'])
+        ];
+
+        foreach($db_config as $key=>$value){
+            putenv($key.'='.$value);
+            $_SERVER[$key] = $value;
+        }
     }
 }
